0b6a7a36f76182b347074db38756d1d6a326aaf0
Danh Tran <danhtran12797@gmail.com>
2022-10-21 09:11:09 +0700

    create page intro

--- android/app/src/main/kotlin/com/example/snackbar_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/snackbar_demo/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity: FlutterActivity() {
             val message: String? = arg?.get("msg")
 
             if(call.method=="showtoast"){
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
                 OmniWallet
                     .with(this)
                     .start()
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -16,21 +16,96 @@ class _MyAppState extends State<MyApp> {
 
   static const Channel = MethodChannel('com.example.snackbar_demo');
 
+  ImageProvider logo = AssetImage("assets/images/onboarding_carousel_2.png");
+
+  @override
+  void didChangeDependencies() {
+    precacheImage(logo, context);
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          fontFamily: 'Quicksand'
+      ),
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Call Native Code"),
-              ElevatedButton(onPressed: (){
-                _showToast();
-              }, child: Text('Start'))
+              SizedBox(
+                height: 85,
+              ),
+              Text(
+                "OMNI WALLET",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Text(
+                "Chào mừng bạn đến với\nOmni Wallet",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Text(
+                  "Omni Wallet là một ví an toàn được hàng triệu người tin dùng, giúp tất cả mọi người tiếp cận dễ dàng với thế giới web3.",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Expanded(
+                  child: Image(
+                      image: logo,
+                  )
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: ElevatedButton(
+                    onPressed: () {
+                      _showToast();
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)
+                      ),
+                      minimumSize: Size.fromHeight(50),
+                    ),
+                    child: Text(
+                      'Bắt đầu',
+                      style: TextStyle(
+                        fontSize: 15
+                      ),
+                    )
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
             ],
           ),
         ),
@@ -38,9 +113,10 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Future<void> _showToast() async{
-    final int showtoast = await Channel.invokeMethod('showtoast', <String, String>{
-      'msg':'This is a toast from Flutter to Android Native'
+  Future<void> _showToast() async {
+    final int showtoast = await Channel.invokeMethod(
+        'showtoast', <String, String>{
+      'msg': 'This is a toast from Flutter to Android Native'
     });
   }
 }
